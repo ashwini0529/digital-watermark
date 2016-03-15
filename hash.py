5c6d413cf3a7dfdd50829c3d62eebe1a12a7d765
Ashwini <geek.ashwini@gmail.com>
2016-03-15 11:29:14 +0530

    Image

--- hash.py
+++ hash.py
@@ -6,7 +6,7 @@ import io
 
 #Convert image to binary image...
 from PIL import Image 
-image_file = Image.open("crawl.png") # open colour image
+image_file = Image.open("test.png") # open colour image
 image_file = image_file.convert('1') # convert image to black and white
 image_file.save('result.png')
 print 'Binary converted'
