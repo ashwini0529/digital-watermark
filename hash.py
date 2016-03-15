980bad38148585e519ad6370a94c21c2d1a2acf1
Ashwini <geek.ashwini@gmail.com>
2016-03-15 11:25:24 +0530

    Binary image and hash generated

---
+++ hash.py
@@ -0,0 +1,22 @@
+import hashlib
+import Image
+import io
+
+
+
+#Convert image to binary image...
+from PIL import Image 
+image_file = Image.open("crawl.png") # open colour image
+image_file = image_file.convert('1') # convert image to black and white
+image_file.save('result.png')
+print 'Binary converted'
+
+#Generate hash value from crated binary image.
+img = Image.open('result.png')
+m = hashlib.md5()
+with io.BytesIO() as memf:
+    img.save(memf, 'PNG')
+    data = memf.getvalue()
+    m.update(data)
+hashedValue = (m.hexdigest()) # gives image's hashed value.
+print hashedValue
